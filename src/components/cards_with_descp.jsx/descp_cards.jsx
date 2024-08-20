317434c22c3fe4237c758a97a10c6cb7362ddd37
ahmednabil14 <ahmednabil14499@gmail.com>
2024-08-20 20:52:02 +0300

    watch list

--- src/components/cards_with_descp.jsx/descp_cards.jsx
+++ src/components/cards_with_descp.jsx/descp_cards.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import contextFavList from '../../context/context';
 import { useContext } from 'react';
+import heartIcon from '../../icons/yellow_heart.png';
 
-const DscpCards = () => {
+const DescpCards = () => {
 
-    const {favList, setFavList} = useContext(contextFavList)
+    const { favList } = useContext(contextFavList);
 
-  return (
-    <Row xs={1} md={2} className="g-4">
-      {favList.map((movie) => (
-        <Col key={movie.id}>
-          <Card>
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movie.title}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">{movie.release_date}</Card.Subtitle>
-              <div className="d-flex align-items-center mb-2">
-                {/* <span className="ms-2">{movie.vote_count}</span> */}
-              </div>
-              <Card.Text>
-                {movie.overview}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  );
+    return (
+        <Row xs={1} md={2} className="g-4">
+            {favList.map((movie) => (
+                <Col key={movie.id}>
+                    <div style={{ position: 'relative', width: '100%', maxWidth: 600, height: 'auto', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 19, padding: 20 }}>
+                        <div style={{ position: 'absolute', top: 10, right: 10, width: 30, height: 30 }}><img src={heartIcon} /></div>
+                        
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <img
+                                style={{ width: 150, height: 220, borderRadius: 20, objectFit: 'cover' }}
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                alt={movie.title}
+                            />
+                            <div style={{ flex: 1, marginLeft: 20 }}>
+                                <div style={{ marginBottom: 10 }}>
+                                    <span style={{ color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '600', lineHeight: '1.2' }}>
+                                        {movie.title}
+                                    </span>
+                                    <br />
+                                    <span style={{ color: '#858585', fontSize: 14, fontFamily: 'Inter', fontWeight: '400' }}>
+                                        {movie.release_date}
+                                    </span>
+                                </div>
+                                <div style={{ display: 'flex', alignItems: 'center', marginTop: 15 }}>
+                                    <div style={{ marginLeft: 10, color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400' }}>
+                                        {movie.vote_count}
+                                    </div>
+                                </div>
+                                <div style={{ color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word', marginTop: 10, maxHeight: 100, overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                    {movie.overview}
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+            ))}
+        </Row>
+    );
 };
 
-export default DscpCards;
+export default DescpCards;
